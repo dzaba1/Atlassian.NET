1dbf87850bcca55b78da0ff9d5597b7b872c6aea
dzaba1 <blazej.zabialowicz@gmail.com>
2025-10-05 12:09:37 +0200

    First cloud integration test

---
+++ Dzaba.AtlassianSdk.Jira.IntegrationTests/IssueCreateTest.cs
@@ -0,0 +1,33 @@
+﻿using Atlassian.Jira;
+using FluentAssertions;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace Dzaba.AtlassianSdk.Jira.IntegrationTests;
+
+[TestFixture]
+public class IssueCreateTest : JiraTestFixture
+{
+    [Test]
+    public async Task CreateIssueWithIssueTypesPerProject()
+    {
+        var issue = new Issue(Jira, TestProjectKey)
+        {
+            Type = "Bug",
+            Summary = "Test Summary " + Rand.Next(int.MaxValue),
+            Assignee = "admin"
+        };
+
+        try
+        {
+            issue.Type.SearchByProjectOnly = true;
+            var newIssue = await issue.SaveChangesAsync();
+
+            newIssue.Project.Should().Be(TestProjectKey);
+        }
+        finally
+        {
+            await DeleteIssueSafeAsync(issue.Key.Value);
+        }
+    }
+}
---
+++ Dzaba.AtlassianSdk.Jira.IntegrationTests/JiraTestFixture.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using System;
+using System.Threading.Tasks;
+
+namespace Dzaba.AtlassianSdk.Jira.IntegrationTests;
+
+public class JiraTestFixture
+{
+    private static readonly string ClouldUrl = "";
+    private static readonly string ClouldUser = "";
+    private static readonly string ClouldToken = "";
+    protected static readonly Random Rand = new Random();
+
+    protected Atlassian.Jira.Jira Jira { get; private set; }
+    protected string TestProjectKey { get; private set; }
+
+    [OneTimeSetUp]
+    public void OneTimeSetupBase()
+    {
+        Jira = Atlassian.Jira.Jira.CreateRestClient(ClouldUrl, ClouldUser, ClouldToken);
+        TestProjectKey = "";
+    }
+
+    protected async Task DeleteIssueSafeAsync(string issueKey)
+    {
+        try
+        {
+            await Jira.Issues.DeleteIssueAsync(issueKey);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+        }
+    }
+}
