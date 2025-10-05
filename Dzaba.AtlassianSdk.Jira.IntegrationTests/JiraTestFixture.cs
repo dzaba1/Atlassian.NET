233a3c6c58075a0c2694c3b531e19cf590892f80
dzaba1 <blazej.zabialowicz@gmail.com>
2025-10-05 12:31:46 +0200

    Logging in tests

--- Dzaba.AtlassianSdk.Jira.IntegrationTests/JiraTestFixture.cs
+++ Dzaba.AtlassianSdk.Jira.IntegrationTests/JiraTestFixture.cs
@@ -1,4 +1,7 @@
-﻿using NUnit.Framework;
+﻿using Atlassian.Jira;
+using Microsoft.Extensions.Logging;
+using NUnit.Framework;
+using Serilog;
 using System;
 using System.Threading.Tasks;
 
@@ -11,14 +14,39 @@ public class JiraTestFixture
     private static readonly string ClouldToken = "";
     protected static readonly Random Rand = new Random();
 
+    private ILoggerFactory loggerFactory;
+
     protected Atlassian.Jira.Jira Jira { get; private set; }
     protected string TestProjectKey { get; private set; }
 
     [OneTimeSetUp]
     public void OneTimeSetupBase()
     {
-        Jira = Atlassian.Jira.Jira.CreateRestClient(ClouldUrl, ClouldUser, ClouldToken);
+        loggerFactory = GetLoggerFactory();
+
+        var settings = new JiraRestClientSettings
+        {
+            LoggerFactory = loggerFactory
+        };
+
+        Jira = Atlassian.Jira.Jira.CreateRestClient(ClouldUrl, ClouldUser, ClouldToken, settings);
         TestProjectKey = "";
+    }
+
+    [OneTimeTearDown]
+    public void OneTimeClenupBase()
+    {
+        loggerFactory?.Dispose();
+    }
+
+    private ILoggerFactory GetLoggerFactory()
+    {
+        var logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .WriteTo.Console()
+            .CreateLogger();
+
+        return LoggerFactory.Create(l => l.AddSerilog(logger, true));
     }
 
     protected async Task DeleteIssueSafeAsync(string issueKey)
