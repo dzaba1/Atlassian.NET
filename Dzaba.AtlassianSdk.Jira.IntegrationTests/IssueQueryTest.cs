f8dc09dc5a51a5727cd3cf52758e25f03c34ee24
dzaba1 <blazej.zabialowicz@gmail.com>
2025-10-05 12:41:13 +0200

    First broken jql test

---
+++ Dzaba.AtlassianSdk.Jira.IntegrationTests/IssueQueryTest.cs
@@ -0,0 +1,27 @@
+﻿using Atlassian.Jira;
+using FluentAssertions;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dzaba.AtlassianSdk.Jira.IntegrationTests;
+
+[TestFixture]
+public class IssueQueryTest : JiraTestFixture
+{
+    [Test]
+    public async Task GetIssueThatIncludesOnlyOneBasicField()
+    {
+        var options = new IssueSearchOptions($"key = {TestProjectKey}-1")
+        {
+            FetchBasicFields = false,
+            AdditionalFields = new List<string>() { "summary" }
+        };
+
+        var issues = await Jira.Issues.GetIssuesFromJqlAsync(options);
+
+        issues.First().Summary.Should().NotBeNullOrEmpty();
+        issues.First().Assignee.Should().BeNull();
+    }
+}
