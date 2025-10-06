4a809d9d60937d1ba5771f84ce5fedefad8f5657
dzaba1 <blazej.zabialowicz@gmail.com>
2025-10-06 19:10:50 +0200

    Moving tests

--- Dzaba.AtlassianSdk.Jira.IntegrationTests/IssueCreateTest.cs
+++ Dzaba.AtlassianSdk.Jira.IntegrationTests/IssueCreateTest.cs
@@ -1,6 +1,7 @@
 ﻿using Atlassian.Jira;
 using FluentAssertions;
 using NUnit.Framework;
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -35,5 +36,93 @@ public class IssueCreateTest : JiraTestFixture
         {
             await DeleteIssueSafeAsync(issue.Key?.Value);
         }
+    }
+
+    [Test]
+    public async Task CreateIssueWithOriginalEstimate()
+    {
+        var fields = new CreateIssueFields(TestProject.Key)
+        {
+            TimeTrackingData = new IssueTimeTrackingData("1d")
+        };
+
+        var issue = new Issue(Jira, fields)
+        {
+            Type = "Bug",
+            Summary = "Test Summary " + Rand.Next(int.MaxValue),
+            Assignee = "admin"
+        };
+
+        try
+        {
+            var newIssue = await issue.SaveChangesAsync();
+
+            newIssue.TimeTrackingData.OriginalEstimate.Should().Be("1d");
+        }
+        finally
+        {
+            await DeleteIssueSafeAsync(issue.Key?.Value);
+        }
+    }
+
+    [Test]
+    public async Task CreateIssueAsync()
+    {
+        var summaryValue = "Test Summary " + Rand.Next(int.MaxValue);
+        var issue = new Issue(Jira, TestProject.Key)
+        {
+            Type = "Bug",
+            Summary = summaryValue,
+            Assignee = "admin"
+        };
+
+        var subTaskType = await Jira.IssueTypes.GetIssueTypesAsync()
+            .FirstAsync(i => i.IsSubTask);
+
+        try
+        {
+            var newIssue = await issue.SaveChangesAsync();
+            newIssue.Summary.Should().Be(summaryValue);
+            newIssue.Project.Should().Be(TestProject.Key);
+
+            // Create a subtask async.
+            var subTask = new Issue(Jira, TestProject.Key, newIssue.Key.Value)
+            {
+                Type = subTaskType,
+                Summary = "My Subtask",
+                Assignee = "admin"
+            };
+
+            var newSubTask = await subTask.SaveChangesAsync();
+
+            newSubTask.ParentIssueKey.Should().Be(newIssue.Key.Value);
+        }
+        finally
+        {
+            await DeleteIssueSafeAsync(issue.Key?.Value);
+        }
+    }
+
+    [Test]
+    public async Task CreateAndQueryIssueWithMinimumFieldsSet()
+    {
+        var summaryValue = "Test Summary " + Rand.Next(int.MaxValue);
+
+        var issue = new Issue(Jira, TestProject.Key)
+        {
+            Type = "Bug",
+            Summary = summaryValue,
+            Assignee = "admin"
+        };
+
+        await issue.SaveChangesAsync();
+
+        var issues = (from i in Jira.Issues.Queryable
+                      where i.Key == issue.Key
+                      select i).ToArray();
+
+        issues.Should().HaveCount(1);
+        issues[0].Summary.Should().Be(summaryValue);
+        issues[0].Project.Should().Be(TestProject.Key);
     }
 }
